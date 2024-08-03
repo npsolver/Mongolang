93f145f9a94b40cc9188a97b05fad84e4f1f8a13
npsolver <raiyanjamil104+5@gmail.com>
2024-08-03 00:15:35 -0400

    added a required filename arg to convert cmd

--- cmd/convert.go
+++ cmd/convert.go
@@ -11,9 +11,10 @@ func init() {
 }
 
 var convertCmd = &cobra.Command{
-	Use: "convert",
+	Use: "convert [filename]",
 	Long: `Takes in a Mongo query through a file or command line
 and converts it into Go code to be used for a go mongo-driver.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fileName := args[0]
 		fileBytes, err := os.ReadFile(fileName)
