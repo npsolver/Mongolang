883d1ab65bd71b134298ec69c7db7a502aabbd88
npsolver <raiyanjamil104+5@gmail.com>
2026-02-28 16:57:20 -0500

    added monaco editor

--- web/app/_components/content.tsx
+++ web/app/_components/content.tsx
@@ -1,15 +1,49 @@
+"use client"
+
 import Pad from "./pad";
+import { useState } from "react";
 
 export default function Content() {
+
+	const [query, setQuery] = useState(`db.users.find({
+    $or: [
+        { age: 
+            { $lt: 18 } 
+        }, 
+        { age: 
+            { $gt: 65 } }
+    ]
+})`)
+	const [code, setCode] = useState(`bson.M{
+    "$or": bson.A{
+        bson.M{
+            "age": bson.M{
+                "$lt": 18
+            }
+        },
+        bson.M{
+            "age": bson.M{
+                "$gt": 65
+            }
+        }
+    }
+}`)
+
 	return (
 		<div className="h-full flex justify-evenly items-center">
 			<Pad
 				background="bg-green-100"
 				title="MongoDB Query"
+				readOnly={false}
+				value={query}
+				setValue={setQuery}
 			/>
 			<Pad
 				background="bg-blue-100"
 				title="Golang Driver Code"
+				readOnly={true}
+				value={code}
+				setValue={setCode}
 			/>
 		</div>
 	)
--- web/app/_components/pad.tsx
+++ web/app/_components/pad.tsx
@@ -1,11 +1,40 @@
-export default function Pad({ background, title }: { background: string, title: string }) {
+import Editor from "@monaco-editor/react";
+
+type padProps = {
+	background: string,
+	title: string,
+	readOnly: boolean,
+	value: string,
+	setValue: (value: string) => void
+}
+
+export default function Pad({
+	background,
+	title,
+	readOnly,
+	value,
+	setValue
+}: padProps) {
+
 	return (
 		<div className={`h-[85vh] w-[45vw] ${background}`}>
 			<div className="flex items-center justify-center h-[5vh]">
 				{title}
 			</div>
 			<div className="h-[80vh] p-4">
-				Bottom
+				<Editor
+					defaultLanguage="python"
+					value={value}
+					onChange={(value, _) => setValue(value ?? "")}
+					theme="light"
+					options={{
+						fontSize: 14,
+						readOnly: readOnly,
+						minimap: { enabled: false },
+						lineNumbers: "on",
+						wordWrap: "on",
+					}}
+				/>
 			</div>
 		</div>
 	)
